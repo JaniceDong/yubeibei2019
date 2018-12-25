1bb10881d621c33b72487efe150a951ad7821523
buwanmei2 <1581996825@qq.com>
2018-12-25 21:48:26 +0800

    chiidwkak

---
+++ ipp2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+  char answer[32];
+  printf ("please input yes or no:\n");
+  gets(answer);
+  int result = strcmp(answer,"yes");
+  if (result == 0) {
+    printf("you have input a yes~\n");
+  } else {
+    printf("you have input a no~\n");
+  }
+  printf ("it over!");
+  return 0;
+}
